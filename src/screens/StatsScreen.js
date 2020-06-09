1af4bca75695f43fe89425d3870e6c78521826d7
root <ismaelgomes@alu.ufc.br>
2020-06-09 14:41:34 -0300

    09/06

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -44,6 +44,17 @@ import Dialog, {
 
 const width = Dimensions.get("window").width;
 
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
+
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 const scale = SCREEN_WIDTH / 320;
 
@@ -81,6 +92,7 @@ class StatsScreen extends React.Component {
       quantidade_lider: [],
       quantidade_semana: null,
       quantidade_mes: null,
+      quantidade_bairro: null,
     };
   }
 
@@ -123,12 +135,14 @@ class StatsScreen extends React.Component {
         quantidade_lider,
         quantidade_semana,
         quantidade_mes,
+        quantidade_bairro,
       } = response.data;
       this.setState({
         quantidade: quantidade,
         quantidade_lider: quantidade_lider,
         quantidade_semana: quantidade_semana,
         quantidade_mes: quantidade_mes,
+        quantidade_bairro: quantidade_bairro,
       });
       /*this.setState({
         totalEleitores: response.data,
@@ -321,6 +335,214 @@ class StatsScreen extends React.Component {
 
         return data;
       };
+      const data2 = [
+        {
+          name: "Nossa Sra. das Graças",
+          population: 21500000,
+          color: "#6681bb",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Canindezinho",
+          population: 2800000,
+          color: "#56b9c3",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Riacho São Francisco",
+          population: 527612,
+          color: "#439185",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Bela Vista",
+          population: 8538000,
+          color: "#316a4c",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Campinas",
+          population: 11920000,
+          color: "#1d4317",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Boa Vista",
+          population: 11920000,
+          color: "#3c631d",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Conjunto Habitacional",
+          population: 11920000,
+          color: "#758427",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Alto Guaramiranga",
+          population: 11920000,
+          color: "#b7a239",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Palestina",
+          population: 11920000,
+          color: "#f5bc58",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Jubaia",
+          population: 11920000,
+          color: "#6eafe1",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "São Mateus",
+          population: 11920000,
+          color: "#5e548d",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Cachoeira da Pasta",
+          population: 11920000,
+          color: "#678ac1",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "João Paulo II",
+          population: 11920000,
+          color: "purple",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Monte",
+          population: 11920000,
+          color: "pink",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Cap. Pedro Sampaio",
+          population: 11920000,
+          color: "red",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Santa Luzia",
+          population: 11920000,
+          color: "orange",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Imaculada Conceição",
+          population: 11920000,
+          color: "grey",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Santa Clara",
+          population: 11920000,
+          color: "black",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Centro",
+          population: 11920000,
+          color: "yellow",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+      ];
+
+      const data3 = [
+        {
+          name: "Bonito",
+          population: 21500000,
+          color: "#6eafe1",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Campos",
+          population: 2800000,
+          color: "#f5bc58",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Caiçara",
+          population: 527612,
+          color: "#b7a239",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Capitão Pedro Sampaio",
+          population: 8538000,
+          color: "#758427",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Esperança",
+          population: 11920000,
+          color: "#3c631d",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Iguaçu",
+          population: 11920000,
+          color: "#1d4317",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Ipueiras dos Gomes",
+          population: 11920000,
+          color: "#316a4c",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Monte Alegre",
+          population: 11920000,
+          color: "#439185",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Salitre",
+          population: 11920000,
+          color: "#56b9c3",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        },
+        {
+          name: "Targinos",
+          population: 11920000,
+          color: "#6681bb",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15,
+        }
+      ];
 
       return (
         <ScrollView style={styles.container}>
@@ -376,6 +598,28 @@ class StatsScreen extends React.Component {
             style={styles.container}
             renderItem={this.renderItem2}
             numColumns={numColumns}
+          />
+          <Text style={styles.title}>Eleitores por bairro</Text>
+          <PieChart
+            data={data2}
+            width={width}
+            height={220}
+            chartConfig={chartConfig}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+            absolute
+          />
+          <Text style={styles.title}>Eleitores por distrito</Text>
+          <PieChart
+            data={data3}
+            width={width}
+            height={220}
+            chartConfig={chartConfig}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+            absolute
           />
         </ScrollView>
       );
